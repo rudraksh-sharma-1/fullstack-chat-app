5a6482c12b83d3fff3792c3082d2395c975afe2d
rudraksh sharma <160497498+rudraksh-sharma-1@users.noreply.github.com>
2025-06-25 10:30:12 +0530

    Update CoverPage.jsx

--- frontend/src/pages/CoverPage.jsx
+++ frontend/src/pages/CoverPage.jsx
@@ -4,11 +4,6 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div className="h-screen bg-cover bg-no-repeat flex items-end pb-40" style={{ backgroundImage: "url('/OsmoLanding.png')" }}>
-        <Link to={'/login'}>  
-        <button type="submit" className="btn btn-primary ml-20 w-48">
-            Get Started
-        </button>
-        </Link>
     </div>
   );
 }
